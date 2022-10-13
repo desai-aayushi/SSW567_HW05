cabc99ce89fd92a7c771162ca7f1c6a0ec97ef88
Aayushi Desai <aayushi1512000@gmail.com>
2022-10-12 22:31:32 -0400

    initial commit

---
+++ Triangle.py
@@ -0,0 +1,26 @@
+def classifyTriangle(a,b,c):
+
+    if a > 200 or b > 200 or c > 200:
+        return 'InvalidInput'
+        
+    if a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
+
+    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+        return 'InvalidInput';
+
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+        return 'NotATriangle'
+
+    if a == b and b == a and c == b:
+        return 'Equilateral'
+    elif ((a ** 2) + (b ** 2)) == (c ** 2):
+        return 'Right'
+    elif ((b ** 2) + (c ** 2)) == (a ** 2):
+        return 'Right'
+    elif ((a ** 2) + (c ** 2)) == (b ** 2):
+        return 'Right'
+    elif (a != b) and  (b != c) and (a != c):
+        return 'Scalene'
+    else:
+        return 'Isoceles'
